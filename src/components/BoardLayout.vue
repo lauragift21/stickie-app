e3fca824640e8cf8b1df370ac499f34de7360916
Gift Egwuenu <laurandidi21@gmail.com>
2021-07-30 22:11:12 +0200

    add prettier config

--- src/components/BoardLayout.vue
+++ src/components/BoardLayout.vue
@@ -1,19 +1,25 @@
 <template>
- <div>
-   <p>Create a New Sticky Button</p>
-   <p>Show all the stickies anywhere on the screen</p>
- </div>
+  <div>
+    <p>Create a New Sticky Button</p>
+    <p>Show all the stickies anywhere on the screen</p>
+    <sticky-card />
+  </div>
 </template>
 
 
 <script>
+import StickyCard from './StickyCard.vue'
+
 export default {
- data() {
-  return {
-    sticky: null
-  };
- },
-};
+  components: {
+    StickyCard,
+  },
+  data() {
+    return {
+      sticky: null,
+    }
+  },
+}
 </script>
 
 
